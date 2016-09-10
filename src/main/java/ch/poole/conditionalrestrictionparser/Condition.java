2b2555f7553edf7ecc7f33b1efd9ca57818f88af
simonpoole <simon@poole.ch>
2016-09-10 09:41:51 +0200

    Added some getters and other aux methods

--- src/main/java/ch/poole/conditionalrestrictionparser/Condition.java
+++ src/main/java/ch/poole/conditionalrestrictionparser/Condition.java
@@ -1,5 +1,8 @@
 package ch.poole.conditionalrestrictionparser;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Container for a OSM condition
  * @author simon
@@ -17,6 +20,7 @@ public class Condition {
 		LTEQ
 	}
 	CompOp op = null;
+	public static List<String> compOpStrings = Arrays.asList("=",">",">=","<","<=");
 	
 	public Condition(String c, boolean OH) {
 		this.c = c; 
@@ -74,5 +78,31 @@ public class Condition {
 			return CompOp.LTEQ;
 		}
 		return null;
+	}
+	
+	/**
+	 * @return true if this is an OH spec
+	 */
+	public boolean isOpeningHours() {
+		return OH;
+	}
+	
+	/**
+	 * @return true if this is an expression
+	 */
+	public boolean isExpression() {
+		return op != null && c2 != null;
+	}
+	
+	public String term1() {
+		return c;
+	}
+	
+	public String term2() {
+		return c2;
+	}	
+	
+	public CompOp operator() {
+		return op;
 	}
 }
